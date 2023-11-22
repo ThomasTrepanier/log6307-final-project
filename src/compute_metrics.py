49e6dc563708998a3b9d40f15b7e4cfaaf1a877f
ThomasTrepanier <thomtrepanier@gmail.com>
2023-11-21 19:36:52 -0500

    compute comment ratio

--- src/compute_metrics.py
+++ src/compute_metrics.py
@@ -3,9 +3,9 @@ from radon.metrics import h_visit, h_visit_ast, mi_visit, HalsteadReport
 import json
 
 
-class CyclomaticComplexity:
+class MetricsComputer:
 
-    def compute_complexity(self, code) -> int:
+    def compute_cyclomatic_complexity(self, code) -> int:
         # Use cc_visit to get a list of complexity results
         try:
             results = cc_visit(code)
@@ -20,3 +20,13 @@ class CyclomaticComplexity:
             total_cognitive_complexity += result.complexity
 
         return total_cognitive_complexity
+
+    def compute_comment_to_code_ratio(self, code: str) -> float:
+        lines = code.split('\n')[:-1]
+        if len(lines) == 0:
+            return 0
+
+        comment_lines = list(
+            filter(lambda line: line.strip().startswith('#'), lines))
+
+        return len(comment_lines) / len(lines)
